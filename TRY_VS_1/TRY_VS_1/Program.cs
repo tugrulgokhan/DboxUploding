f5be8104b512668f5f9e43eb3d78b762530af72b
tugrulgokhan <34692501+tugrulgokhan@users.noreply.github.com>
2019-06-24 10:53:58 +0300

    Add files via upload

---
+++ TRY_VS_1/TRY_VS_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dropbox.Api;
+using System.IO;
+using Dropbox.Api.Files;
+
+
+namespace TRY_VS_1
+{
+    class Program
+    {
+        static string token = "...";
+
+        static void Main(string[] args)
+        {
+            var task = Task.Run((Func<Task>)Program.Run);
+
+            Console.ReadLine();
+        }
+
+        static async Task Run()
+        {
+
+            using (var dbx = new DropboxClient(token))
+            {
+                string file = @"...";
+                string fileNa = Path.GetFileName(file);
+
+                string fileName = "postApp.jpg";
+                string folder = "/DB";
+                string url = "";
+
+                using (var memory = new MemoryStream(File.ReadAllBytes(fileNa)))
+                {
+                    var updated = dbx.Files.UploadAsync(folder + "/" + fileName, WriteMode.Overwrite.Instance, body: memory);
+                    updated.Wait();
+                    var tx = dbx.Sharing.CreateSharedLinkWithSettingsAsync(folder + "/" + fileName);
+                    tx.Wait();
+                    url = tx.Result.Url;
+
+                }
+
+                Console.WriteLine(url);
+            }
+        }
+    }
+}
+
